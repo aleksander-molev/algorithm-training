a247ec62a2e32ad119e7fc3d1a8e789ad818e60a
Aleksander <aleksander-molev@yandex.ru>
2026-09-12 21:51:16 +0300

    Add Java 21 Gradle build

---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    java
+}
+
+group = "dev.alex.algorithmtraining"
+version = "1.0-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.11.4"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
