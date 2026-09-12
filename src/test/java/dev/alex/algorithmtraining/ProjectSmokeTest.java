037357ad9024188d4f95c101764c38dea22d832b
Aleksander <aleksander-molev@yandex.ru>
2026-09-12 21:51:46 +0300

    Add project smoke test

---
+++ src/test/java/dev/alex/algorithmtraining/ProjectSmokeTest.java
@@ -0,0 +1,13 @@
+package dev.alex.algorithmtraining;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class ProjectSmokeTest {
+
+    @Test
+    void projectIsConfigured() {
+        assertTrue(true);
+    }
+}
